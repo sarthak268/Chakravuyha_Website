d704e6cf14506270ae1fa74b19ef7382df06a2d2
sarthak268 <sarthakbhagat@Sarthaks-MacBook-Pro.local>
2017-07-27 22:36:40 +0530

    last

--- app.py
+++ app.py
@@ -58,16 +58,17 @@ def xy():
 def login():
     form = LoginForm()
 
-    if form.validate_on_submit():
+    '''if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user)
                 return redirect(url_for('dashboard'))
 
-        return '<h1>Invalid username or password</h1>'
+        return '<h1>Invalid username or password</h1>
 
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form)'''
+    return render_template('blank.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
